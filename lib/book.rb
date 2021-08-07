65b2d976d6f2f3c7aa736a28ed5d0acd179fa7a5
vincent <vincent.arteaga@gmail.com>
2021-08-06 18:36:27 -0700

    lab complete

--- lib/book.rb
+++ lib/book.rb
@@ -1,9 +1,15 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
-
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
+  # constant genres
+  GENRES = []
   def initialize(title)
     @title = title
+  end
+
+  def genre=(new_genre)
+    @genre = genre
+    GENRES << new_genre
   end
 
   def turn_page
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,8 +2,19 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+    if !BRANDS.include?(brand)
+    BRANDS << brand
+    end
+  end
+
+  def brand=(new_brand)
+
+    BRANDS << new_brand
+    BRANDS.uniq
   end
 
   def cobble
